f866204ab0729df710b3a7ad11f0d4e204236ac2
Ali Camarata <alisalaah@gmail.com>
2023-11-03 11:28:56 +0700

    added meta to finish

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 // src/pages/_app.tsx
 import React from 'react';
 import { AppProps } from 'next/app';
+import Head from 'next/head';  // Import the Head component from next/head
 import Layout from '../components/Layout';
 import '../styles/globals.css';
 
@@ -14,6 +15,13 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   return (
     <Layout>
+      <Head>
+        <title>Unity Devs - Pioneering Digital Solutions for Everyone</title>
+        <meta
+          name="description"
+          content="Unity Devs is a forward-thinking software agency delivering top-tier digital solutions to Fortune 500 firms and pioneering a unique charitable initiative. Our innovative projects not only drive business transformation but also contribute to a meaningful social impact through our Endowment-as-a-Service (EaaS). Engage with us for a blend of technological excellence and a step towards bettering the world."
+        />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
